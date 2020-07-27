4837170223df56c040dea285d9099cddaeae9471
Lukas <lukasimbm@gmail.com>
2020-07-27 12:20:46 +0300

    video 46

---
+++ resources/views/payment/index.blade.php
@@ -0,0 +1,11 @@
+@extends('video15.layout')
+
+@section('content')
+    <div id="wrapper">
+        <div id="page" class="container">
+            <div id="content">
+                <a class="btn btn-primary" href="/payments" role="button">Make Payment</a>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -87,3 +87,6 @@ Route::get('/home', 'HomeController@index')
 
 Route::get('/contact', 'ContactController@show');
 Route::post('/contact', 'ContactController@store');
+
+Route::get('payments/create', 'PaymentsController@create')->middleware('auth');
+Route::post('payments', 'PaymentsController@store')->middleware('auth');
